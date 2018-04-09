606c976692d1b2586de41153b22a5542f4534ba7
jdz <jdz@1.com>
2018-04-09 12:32:55 +0800

    complete best_login

--- login.mjs
+++ login.mjs
@@ -24,6 +24,7 @@ export default class login{
                 })
            },(reason)=>{
                console.log("login failed");
+               driver.quit();
            })
        })
    }
