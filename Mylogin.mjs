3256ac2136c5b3569bde9c320225393e42f780a6
jdz <jdz@1.com>
2018-04-09 11:09:23 +0800

    complete login

---
+++ Mylogin.mjs
@@ -0,0 +1,7 @@
+import login from "./login.mjs";
+
+let username="jdzhang@nltechdev.com";
+let password="111111";
+
+let objLogin=new login(username,password);
+objLogin.userLogin();
---
+++ login.mjs
@@ -0,0 +1,25 @@
+import webdriver from "selenium-webdriver";
+
+export default class login{
+    constructor(username,password){
+       this.username=username;
+       this.password=password;
+    }
+   userLogin(){
+       let driver=new webdriver.Builder().forBrowser("chrome").build();
+       const msg_url="https://printshopstaging.lifetouch.com/login";
+       driver.get(msg_url);
+       driver.wait(webdriver.until.urlIs(msg_url),2000*10)
+       .then((success)=>{
+           driver.findElement(webdriver.By.xpath("//form[@id='login_form']//input[@id='Email']")).sendKeys(this.username);
+           driver.findElement(webdriver.By.id("Password")).sendKeys(this.password);
+           driver.findElement(webdriver.By.className("button-1 login-button btn btn-default"))
+           .click().then((success)=>{
+               driver.findElement(webdriver.By.partialLinkText("Welcome"));
+               console.log("Login successfully");
+           },(reason)=>{
+               console.log("login failed");
+           })
+       })
+   }
+}
