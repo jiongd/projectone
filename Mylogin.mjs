f494c343c1eecb34f7adae5863ab6e52dbcd848d
jdz <jdz@1.com>
2018-04-10 10:41:33 +0800

    complete best_login

--- Mylogin.mjs
+++ Mylogin.mjs
@@ -1,8 +1,10 @@
 import login from "./login.mjs";
+import webdriver from "selenium-webdriver";
 
 let username="jdzhang@nltechdev.com";
 let password="111111";
 const msg_url="https://printshopstaging.lifetouch.com/login";
+let driver=new webdriver.Builder().forBrowser("chrome").build(); 
 
-let objLogin=new login(username,password);
+let objLogin=new login(username,password,driver);
 objLogin.userLogin(msg_url);
--- login.mjs
+++ login.mjs
@@ -1,30 +1,30 @@
 import webdriver from "selenium-webdriver";
 
 export default class login{
-    constructor(username,password){
+    constructor(username,password,driver){
        this.username=username;
        this.password=password;
+       this.driver=driver;
     }
    userLogin(url){
-       let driver=new webdriver.Builder().forBrowser("chrome").build(); 
-       driver.get(url);
-       driver.wait(webdriver.until.urlIs(url),2000*10)
+       this.driver.get(url);
+       this.driver.wait(webdriver.until.urlIs(url),2000*10)
        .then((success)=>{
-           driver.findElement(webdriver.By.xpath("//form[@id='login_form']//input[@id='Email']")).sendKeys(this.username);
-           driver.findElement(webdriver.By.id("Password")).sendKeys(this.password);
-          driver.findElement(webdriver.By.className("button-1 login-button btn btn-default"))
+           this.driver.findElement(webdriver.By.xpath("//form[@id='login_form']//input[@id='Email']")).sendKeys(this.username);
+           this.driver.findElement(webdriver.By.id("Password")).sendKeys(this.password);
+           this.driver.findElement(webdriver.By.className("button-1 login-button btn btn-default"))
            .click().then((success)=>{
-               driver.findElement(webdriver.By.partialLinkText("Welcome")).getText().then((message)=>{
+               this.driver.findElement(webdriver.By.partialLinkText("Welcome")).getText().then((message)=>{
                 let msg=message;
                 if(/welcome/i.test(msg)){
                     console.log(`login message is ${msg}`);
                     console.log("Login successfully");
-                  //  driver.quit();
+                    this.driver.quit();
                   } 
                 })
            },(reason)=>{
                console.log("login failed");
-              // driver.quit();
+               this.driver.quit();
            })
        })
    }
