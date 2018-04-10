ddfa86dc6d454f54f6b10851be8f5c921414445c
jdz <jdz@1.com>
2018-04-10 10:07:23 +0800

    complete best_login

--- login.mjs
+++ login.mjs
@@ -12,19 +12,19 @@ export default class login{
        .then((success)=>{
            driver.findElement(webdriver.By.xpath("//form[@id='login_form']//input[@id='Email']")).sendKeys(this.username);
            driver.findElement(webdriver.By.id("Password")).sendKeys(this.password);
-           driver.findElement(webdriver.By.className("button-1 login-button btn btn-default"))
+          driver.findElement(webdriver.By.className("button-1 login-button btn btn-default"))
            .click().then((success)=>{
                driver.findElement(webdriver.By.partialLinkText("Welcome")).getText().then((message)=>{
                 let msg=message;
                 if(/welcome/i.test(msg)){
                     console.log(`login message is ${msg}`);
                     console.log("Login successfully");
-                    driver.quit();
+                  //  driver.quit();
                   } 
                 })
            },(reason)=>{
                console.log("login failed");
-               driver.quit();
+              // driver.quit();
            })
        })
    }
